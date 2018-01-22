dcf7086befd34d6aa56d7d474aac01608fdfe5cf
Unknown <mkim2@andrew.cmu.edu>
2018-01-22 14:44:10 -0500

    week1 setup

---
+++ week1/Assets/Scripts/Util/PrefabDB.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Prefab DB")]
+public class PrefabDB : ScriptableObject {
+    [SerializeField]
+    private GameObject[] scenes;
+    public GameObject[] Scenes { get { return scenes; } }
+
+}
