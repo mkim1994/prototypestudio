4cf7cc42f6885b7ba265c121facc49e8440770cb
Unknown <mkim2@andrew.cmu.edu>
2018-01-31 12:56:58 -0500

    week 2 setup

---
+++ week2/Assets/Scripts/Util/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+    /// Control the position of the camera and its behaviour
+    /** Camera should have minPosition and maxPosition of the
+     * same because we're dealing with 2D. The movement speed
+     * shouldn't be too fast nor too slow
+     */
+public class CameraFollow : MonoBehaviour
+{
+
+    /// Target of the camera
+    public Transform target;
+
+    /// Minimum position of camera
+    public float minPosition = -5.3f;
+
+    /// Maximum position of camera
+    public float maxPosition = 5.3f;
+
+    /// Movement speed of camera
+    public float moveSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        var newPosition = Vector3.Lerp(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+        newPosition.x = Mathf.Clamp(newPosition.x, minPosition, maxPosition);
+        newPosition.y = transform.position.y;
+        newPosition.z = transform.position.z;
+
+        transform.position = newPosition;
+    }
+}
+
+
---
+++ week2/Assets/Scripts/Util/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject sceneRoot;
+    public Camera currentCamera;
+    public GameObject TestingScenes;
+	void Awake()
+	{
+        TestingScenes.SetActive(false);
+		InitializeServices();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		//Services.EventManager.Register<Reset>(Reset);
+		Services.SceneStackManager.PushScene<TitleScreen>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Services.TaskManager.Update();
+
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            Application.Quit();
+        }
+
+        if(Input.GetKeyUp(KeyCode.R)){
+            Services.SceneStackManager.Swap<TitleScreen>();
+        }
+	}
+
+	void InitializeServices()
+	{
+		Services.GameManager = this;
+		Services.EventManager = new EventManager();
+		Services.TaskManager = new TaskManager();
+		Services.Prefabs = Resources.Load<PrefabDB>("Prefabs/Prefabs");
+        Services.Materials = Resources.Load<MaterialDB>("Art/Materials");
+		Services.SceneStackManager = new SceneStackManager<TransitionData>(sceneRoot, Services.Prefabs.Scenes);
+		Services.InputManager = new InputManager();
+
+
+	}
+
+	void Reset(Reset e)
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+    //UI buttons
+
+}
