46dfd115426232ae490e7d46ad67691f967fcc8a
Unknown <mkim2@andrew.cmu.edu>
2018-01-31 15:01:34 -0500

    week 2 room + joints + tire

---
+++ week2/Assets/Scripts/TrunkControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunkControl : MonoBehaviour{
+    private Vector3 mousePosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = mousePosition;
+        //mousePosition -= lastMousePos;
+	}
+}
