10b1087bdc1d77c3165d072c4de14b852fd90fae
Unknown <mkim2@andrew.cmu.edu>
2018-03-06 22:59:10 -0500

    week 7 some more

--- week7/Assets/Scripts/SceneScript/Main.cs
+++ week7/Assets/Scripts/SceneScript/Main.cs
@@ -6,14 +6,46 @@ using UnityEngine.UI;
 
 public class Main : Scene<TransitionData> {
 
+    public bool outlineMode;
+    public GameObject ParticleSystem;
+    public GameObject Floor;
+    private Vector3 initialFloorAngles;
 	// Use this for initialization
 	void Start () {
-
+        initialFloorAngles = Floor.transform.localEulerAngles;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if(outlineMode){
+            if(!Services.GameManager.currentCamera.GetComponent<ReplacementShaderEffect>().enabled){
+                Services.GameManager.currentCamera.GetComponent<ReplacementShaderEffect>().enabled = true;
+                Services.GameManager.currentCamera.GetComponent<QuickGlow>().enabled = true;
+                //Floor.GetComponent<Animator>().StopPlayback();
+                StopFloor(true);
+            }
+        } else{
+            if (Services.GameManager.currentCamera.GetComponent<ReplacementShaderEffect>().enabled)
+            {
+                Services.GameManager.currentCamera.GetComponent<ReplacementShaderEffect>().enabled = false;
+                Services.GameManager.currentCamera.GetComponent<QuickGlow>().enabled = false;
+                StopFloor(false);
+
+            }
+        }
+	}
+
+	public void StopFloor(bool stop)
+	{
+        if(stop){
+
+            Floor.GetComponent<Animator>().enabled = false;
+            Floor.transform.localEulerAngles = initialFloorAngles;
+        } else{
+
+            Floor.GetComponent<Animator>().enabled = true;
+        }
 	}
 
 	void InitializeServices()
---
+++ week7/Assets/Scripts/Underwater.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Underwater : MonoBehaviour {
+
+    float waterLevel;
+    public bool isUnderwater;
+    public Color normalColor;
+    public Color underwaterColor;
+
+    private float currentFogDensity;
+	// Use this for initialization
+	void Start () {
+
+        RenderSettings.fogColor = underwaterColor;
+        currentFogDensity = 0f;
+
+        Services.Main.ParticleSystem.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!Services.Main.outlineMode)
+        {
+            if (isUnderwater)
+            {
+               // Services.Main.StopFloor(true);
+                Services.Main.ParticleSystem.SetActive(true);
+                SetUnderWater();
+
+            }
+            else
+            {
+
+
+                Services.Main.ParticleSystem.SetActive(false);
+                SetNormal();
+            }
+        }
+	}
+
+    void SetNormal(){
+        if (currentFogDensity > 0f)
+        {
+            //RenderSettings.fogDensity = 0f;
+            currentFogDensity -= 0.0001f;
+            RenderSettings.fogDensity = currentFogDensity;
+        } else{
+            //Services.Main.StopFloor(false);
+        }
+    }
+
+    void SetUnderWater(){
+        if (currentFogDensity < 0.0024f)
+        {
+            currentFogDensity += 0.0001f;
+            RenderSettings.fogDensity = currentFogDensity;
+        } else{
+
+        }
+    }
+
+}
