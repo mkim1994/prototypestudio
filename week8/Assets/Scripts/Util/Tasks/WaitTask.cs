ad94509bfa7dfc425b994397690faa7a3cae8baa
Unknown <mkim2@andrew.cmu.edu>
2018-03-26 15:02:05 -0400

    week 8 set up

---
+++ week8/Assets/Scripts/Util/Tasks/WaitTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class WaitTask : Task
+{
+    private float duration;
+    private float timeElapsed;
+    public bool finished;
+
+    public WaitTask(float _duration)
+    {
+        duration = _duration;
+    }
+
+    protected override void Init()
+    {
+        timeElapsed = 0;
+    }
+
+    internal override void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= duration) {
+            SetStatus(TaskStatus.Success);
+        }
+    }
+
+    protected override void OnSuccess()
+    {
+        finished = true;
+    }
+
+}
