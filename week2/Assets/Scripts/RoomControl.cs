7e810cdf2628a668046c2fbf32d5f1de89f17bee
Unknown <mkim2@andrew.cmu.edu>
2018-03-26 14:48:02 -0400

    week 2 edit

--- week2/Assets/Scripts/RoomControl.cs
+++ week2/Assets/Scripts/RoomControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomControl : MonoBehaviour {
-    float speed = 50f;
+    float speed = 100f;
     public GameObject goal;
     private float currentAngle;
     // Use this for initialization
@@ -37,20 +37,20 @@ public class RoomControl : MonoBehaviour {
         float v = speed * Input.GetAxis("Mouse Y");
         transform.Rotate(v, h, 0);*/
 
-       /* float h = Input.GetAxis("Mouse X") * speed * Mathf.Deg2Rad;
+        float h = Input.GetAxis("Mouse X") * speed * Mathf.Deg2Rad;
         float v = Input.GetAxis("Mouse Y") * speed * Mathf.Deg2Rad;
 
         transform.Rotate(Vector3.down,h);
-        transform.Rotate(Vector3.right,v);*/
+        transform.Rotate(Vector3.right,v);
 
         //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        //Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
 
         //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        //Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
         //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+       // float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
         /*if(currentAngle < angle){
             currentAngle += angleIncrements;
         } else if(currentAngle > angle){
@@ -58,7 +58,8 @@ public class RoomControl : MonoBehaviour {
         }*/
         // currentAngle = Mathf.Lerp(currentAngle,angle,1f);
         //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, currentAngle));
+       // transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, currentAngle));
+       // Debug.Log("what");
     }
 
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
