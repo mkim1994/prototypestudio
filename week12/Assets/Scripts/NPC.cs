9b8091734016d48723fc07a6d18442ebc9fded9f
Unknown <mkim2@andrew.cmu.edu>
2018-05-07 13:50:34 -0400

    week 12 iteration 2

--- week12/Assets/Scripts/NPC.cs
+++ week12/Assets/Scripts/NPC.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class NPC : MonoBehaviour {
 
     public string charName;
-
+    public Dictionary<NPC,string> toRespondTo = new Dictionary<NPC, string>();
 
     public enum Relationship{
         FriendsWith,
@@ -18,12 +18,15 @@ public class NPC : MonoBehaviour {
     public Relationship[] relationshipStates;
     public NPC[] targets;
 
+    public int[] points;
+
     private Text nameText;
     private Text relationText;
     //SpriteRenderer sprite;
     // Use this for initialization
     GameObject info;
 	void Start () {
+        
 	}
 	
 	// Update is called once per frame
@@ -44,6 +47,7 @@ public class NPC : MonoBehaviour {
 
     public void SetRelationships(List<NPC> ch, Relationship[] state){
         targets = new NPC[ch.Count];
+        points = new int[ch.Count];
         for (int j = 0; j < ch.Count; ++j){
             targets[j] = ch[j];
         }
@@ -80,6 +84,90 @@ public class NPC : MonoBehaviour {
                 relationText.text += relation + ", ";
             }
         }
+    }
+
+    public void PublishRelationship(){
+        relationText.text = "";
+        string relation = "";
+        for (int i = 0; i < relationshipStates.Length; ++i)
+        {
+
+            switch (relationshipStates[i])
+            {
+                case Relationship.Likes:
+                    relation = "Likes " + targets[i].charName;
+                    break;
+                case Relationship.Acquaintance:
+                    relation = "Kind of knows " + targets[i].charName;
+                    break;
+                case Relationship.Dislikes:
+                    relation = "Dislikes " + targets[i].charName;
+                    break;
+                case Relationship.Strangers:
+                    relation = "Doesn't know " + targets[i].charName;
+                    break;
+                case Relationship.FriendsWith:
+                    relation = "Friends with " + targets[i].charName;
+                    break;
+            }
+            if (i == relationshipStates.Length - 1)
+            {
+                relationText.text += relation;
+            }
+            else
+            {
+                relationText.text += relation + ", ";
+            }
+        }
+    }
+
+    public void EvalRelationship(){
+        for (int i = 0; i < relationshipStates.Length; ++i){
+            switch(relationshipStates[i]){
+                case Relationship.Acquaintance:
+                    if(points[i] > 2){
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.FriendsWith;
+                    } else if(points[i] < -2){
+
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.Dislikes;
+                    }
+                    break;
+                case Relationship.Dislikes:
+                    if (points[i] > 5)
+                    {
+
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.Acquaintance;
+                    }
+                    break;
+                case Relationship.Strangers:
+                    if (points[i] > 3)
+                    {
+
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.Acquaintance;
+                    } else if(points[i] < -1){
+
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.Dislikes;
+                    }
+                    break;
+                case Relationship.FriendsWith:
+                    if(points[i] > 3){
+
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.Likes;
+                    } else if(points[i]< -2){
+
+                        points[i] = 0;
+                        relationshipStates[i] = Relationship.Acquaintance;
+                    }
+                    break;
+            }
+        }
+        PublishRelationship();
     }
 
 	private void OnMouseOver()
--- week12/Assets/Scripts/SceneScript/Main.cs
+++ week12/Assets/Scripts/SceneScript/Main.cs
@@ -9,6 +9,8 @@ public class Main : Scene<TransitionData> {
     public int numChars;
     public List<NPC> npcs;
 
+    bool gameEnd;
+
     public NPC.Relationship[] relationshipStates = {NPC.Relationship.Acquaintance, NPC.Relationship.FriendsWith,
         NPC.Relationship.Likes,NPC.Relationship.Dislikes,NPC.Relationship.Strangers};
 
@@ -26,8 +28,14 @@ public class Main : Scene<TransitionData> {
 
     public GameObject nextButton;
     public GameObject[] buttons;
+    public Text episodeNumText;
+    public int episodeNum;
 
     public List<string> MessagePool;
+
+    public Dictionary<NPC.Relationship,string[]> ReactionPoolTalk = new Dictionary<NPC.Relationship,string[]>();
+    public Dictionary<NPC.Relationship, string[]> ReactionPoolLook = new Dictionary<NPC.Relationship, string[]>();
+    //public Dictionary<NPC.Relationship,string> ReactionPool
 
     private string[] words;
 
@@ -37,10 +45,35 @@ public class Main : Scene<TransitionData> {
 	}
 	void Start () {
 
+        gameEnd = false;
         npcs = new List<NPC>();
         currChar = 0;
+        episodeNum = 1;
 
         StartCoroutine(ReadFileAsync("https://gist.githubusercontent.com/mkim1994/c64302fdeac21771c97c952b2f55eb69/raw/884c1a0e01a0c7c2dc7bea906180e840893055aa/names.txt"));
+
+        string[] talk1 = { "replies with disinterest to","replies politely to","replies with some interest to"};
+        string[] talk2 = { "ignores", "replies half-heartedly to ","replies with some interest to"};
+        string[] talk3 = { "is weirded out by", "responds to", "excitedly responds to" };
+        string[] talk4 = { "is weirded out by", "responds to", "ecstatically responds to" };
+        string[] talk5 = { "ignores","replies cautiously to","replies with some interest to"};
+
+        ReactionPoolTalk.Add(NPC.Relationship.Acquaintance, talk1);
+        ReactionPoolTalk.Add(NPC.Relationship.Dislikes, talk2);
+        ReactionPoolTalk.Add(NPC.Relationship.FriendsWith, talk3);
+        ReactionPoolTalk.Add(NPC.Relationship.Likes, talk4);
+        ReactionPoolTalk.Add(NPC.Relationship.Strangers, talk5);
+
+        string[] look1 = { "casts a weird look at", "looks back at", "looks back with interest at" };
+        string[] look2 = { "is creeped out by", "ignores", "looks back at" };
+        string[] look3 = { "is weirded out by", "looks back at", "nods back with approval at" };
+        string[] look4 = { "is weirded out by", "checks out", "shyly checks out" };
+        string[] look5 = { "is creeped out by", "furtively glances back at", "glances back with interest at" };
+        ReactionPoolLook.Add(NPC.Relationship.Acquaintance, look1);
+        ReactionPoolLook.Add(NPC.Relationship.Dislikes, look2);
+        ReactionPoolLook.Add(NPC.Relationship.FriendsWith, look3);
+        ReactionPoolLook.Add(NPC.Relationship.Likes,look4);
+        ReactionPoolLook.Add(NPC.Relationship.Strangers, look5);
 
 	}
 
@@ -100,25 +133,34 @@ public class Main : Scene<TransitionData> {
 	
 	// Update is called once per frame
 	void Update () {
-        if(currChar == numChars){
-            if (nextButton.activeSelf)
+        if (!gameEnd)
+        {
+            if (currChar == numChars)
             {
-                nextButton.SetActive(false);
-            }
-            if(!buttons[0].activeSelf){
-                foreach(GameObject b in buttons){
-                    b.SetActive(true);
+                if (nextButton.activeSelf)
+                {
+                    nextButton.SetActive(false);
+                }
+                if (!buttons[0].activeSelf)
+                {
+                    foreach (GameObject b in buttons)
+                    {
+                        b.SetActive(true);
+                    }
                 }
             }
-        } else{
-            if(!nextButton.activeSelf){
-                nextButton.SetActive(true);
-            }
-            if (buttons[0].activeSelf)
+            else
             {
-                foreach (GameObject b in buttons)
+                if (!nextButton.activeSelf)
                 {
-                    b.SetActive(false);
+                    nextButton.SetActive(true);
+                }
+                if (buttons[0].activeSelf)
+                {
+                    foreach (GameObject b in buttons)
+                    {
+                        b.SetActive(false);
+                    }
                 }
             }
         }
@@ -152,9 +194,249 @@ public class Main : Scene<TransitionData> {
     public void NextAction(){
         string message = "";
         if(currChar < numChars){
-            message += npcs[currChar].charName + " "+ MessagePool[Random.Range(0, MessagePool.Count)]
-                                     + " " + npcs[currChar].targets[Random.Range(0, npcs[currChar].targets.Length)].charName;
+            int count = 0;
+            foreach (KeyValuePair<NPC,string> entry in npcs[currChar].toRespondTo)
+            {
+                count++;
+                // do something with entry.Value or entry.Key
+                //message+=npcs[currChar].charName + " "+
+                if (count > 1)
+                {
+                    message += ". " + npcs[currChar].charName + " ";
+                } else{
+                    message += npcs[currChar].charName + " ";
+                }
+                string reaction = "";
 
+                /*
+                 * if you're talked to/looked at by someone you dislike, 30-70 chance at -1
+                 * 
+                 * 
+                 * */
+                if(entry.Value.Contains("talk")){
+                    for (int i = 0; i < npcs[currChar].targets.Length; ++i){
+                        if(npcs[currChar].targets[i] == entry.Key){
+                            switch(npcs[currChar].relationshipStates[i]){
+                                case NPC.Relationship.Likes:
+                                    string[] strs1;
+                                    int index1;
+                                    float odds1 = Random.Range(0f, 100f);
+                                    if(odds1 > 1f){
+                                        npcs[currChar].points[i]++;
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index1 = 2;
+                                    } else{
+                                        npcs[currChar].points[i]--;
+                                        index1 = 0;
+                                    }
+                                    ReactionPoolTalk.TryGetValue(NPC.Relationship.Likes, out strs1);
+                                    reaction = strs1[index1];
+                                    break;
+                                case NPC.Relationship.Acquaintance:
+                                    string[] strs2;
+                                    int index2;
+                                    float odds2 = Random.Range(0f, 100f);
+                                    if (odds2 > 10f)
+                                    {
+                                        npcs[currChar].points[i]++;
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index2 = 2;
+                                    }else
+                                    {
+                                        npcs[currChar].points[i]--;
+                                        index2 = 0;
+                                    }
+                                    ReactionPoolTalk.TryGetValue(NPC.Relationship.Acquaintance, out strs2);
+                                    reaction = strs2[index2];
+                                    break;
+                                case NPC.Relationship.Dislikes:
+                                    string[] strs3;
+                                    int index3;
+                                    float odds3 = Random.Range(0f, 100f);
+                                    if (odds3 > 30f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index3 = 2;
+                                    } else
+                                    {
+                                        npcs[currChar].points[i]--;
+                                        index3 = 0;
+                                    }
+                                    ReactionPoolTalk.TryGetValue(NPC.Relationship.Dislikes, out strs3);
+                                    reaction = strs3[index3];
+                                    break;
+                                case NPC.Relationship.Strangers:
+                                    string[] strs4;
+                                    int index4;
+                                    float odds4 = Random.Range(0f, 100f);
+                                    if (odds4 > 20f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index4 = 2;
+                                    }else
+                                    {
+                                        npcs[currChar].points[i]--;
+                                        index4 = 0;
+                                    }
+                                    ReactionPoolTalk.TryGetValue(NPC.Relationship.Strangers, out strs4);
+                                    reaction = strs4[index4];
+                                    break;
+                                case NPC.Relationship.FriendsWith:
+                                    string[] strs5;
+                                    int index5;
+                                    float odds5 = Random.Range(0f, 100f);
+                                    if (odds5 > 5f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index5 = 2;
+                                    }else
+                                    {
+
+                                        npcs[currChar].points[i]--;
+                                        index5 = 0;
+                                    }
+                                    ReactionPoolTalk.TryGetValue(NPC.Relationship.FriendsWith, out strs5);
+                                    reaction = strs5[index5];
+                                    break;
+                            }
+                            npcs[currChar].EvalRelationship();
+
+                            //npcs[currChar].toRespondTo.Remove(entry.Key);
+
+                            //break;
+                        }
+                    }
+                } else if(entry.Value.Contains("look")){
+                    for (int i = 0; i < npcs[currChar].targets.Length; ++i)
+                    {
+                        if (npcs[currChar].targets[i] == entry.Key)
+                        {
+                            switch (npcs[currChar].relationshipStates[i])
+                            {
+                                case NPC.Relationship.Likes:
+                                    string[] strs1;
+                                    int index1;
+                                    float odds1 = Random.Range(0f, 100f);
+                                    if (odds1 > 5f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index1 = 2;
+                                    }else
+                                    {
+
+                                        npcs[currChar].points[i]--;
+                                        index1 = 0;
+                                    }
+                                    ReactionPoolLook.TryGetValue(NPC.Relationship.Likes, out strs1);
+                                    reaction = strs1[index1];
+                                    break;
+                                case NPC.Relationship.Acquaintance:
+                                    string[] strs2;
+                                    int index2;
+                                    float odds2 = Random.Range(0f, 100f);
+                                    if (odds2 > 20f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index2 = 2;
+                                    }else
+                                    {
+                                        npcs[currChar].points[i]--;
+                                        index2 = 0;
+                                    }
+                                    ReactionPoolLook.TryGetValue(NPC.Relationship.Acquaintance, out strs2);
+                                    reaction = strs2[index2];
+                                    break;
+                                case NPC.Relationship.Dislikes:
+                                    string[] strs3;
+                                    int index3;
+                                    float odds3 = Random.Range(0f, 100f);
+                                    if (odds3 > 60f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index3 = 2;
+                                    } else
+                                    {
+                                        npcs[currChar].points[i]--;
+                                        index3= 0;
+                                    }
+                                    ReactionPoolLook.TryGetValue(NPC.Relationship.Dislikes, out strs3);
+                                    reaction = strs3[index3];
+                                    break;
+                                case NPC.Relationship.Strangers:
+                                    string[] strs4;
+                                    int index4;
+                                    float odds4 = Random.Range(0f, 100f);
+                                    if (odds4 > 50f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index4 = 2;
+                                    }else
+                                    {
+                                        npcs[currChar].points[i]--;
+                                        index4 = 0;
+                                    }
+                                    ReactionPoolLook.TryGetValue(NPC.Relationship.Strangers, out strs4);
+                                    reaction = strs4[index4];
+                                    break;
+                                case NPC.Relationship.FriendsWith:
+                                    string[] strs5;
+                                    int index5;
+                                    float odds5 = Random.Range(0f, 100f);
+                                    if (odds5 > 10f)
+                                    {
+                                        npcs[currChar].points[i]++;
+
+                                        npcs[currChar].targets[i].points[i]++;
+                                        index5 = 2;
+                                    }else
+                                    {
+
+                                        npcs[currChar].points[i]--;
+                                        index5 = 0;
+                                    }
+                                    ReactionPoolLook.TryGetValue(NPC.Relationship.FriendsWith, out strs5);
+                                    reaction = strs5[index5];
+                                    break;
+                            }
+
+                            npcs[currChar].EvalRelationship();
+                            //npcs[currChar].toRespondTo.Remove(entry.Key);
+                            //break;
+                        }
+                    }
+                }
+                message += reaction;
+                message += " " + entry.Key.charName;
+
+               // break;
+            }
+
+
+            if (count < 1)
+            {
+                int m = Random.Range(0, MessagePool.Count);
+                int t = Random.Range(0, npcs[currChar].targets.Length);
+                message += npcs[currChar].charName + " " + MessagePool[m]
+                                         + " " + npcs[currChar].targets[t].charName;
+                npcs[currChar].targets[t].toRespondTo.Add(npcs[currChar], MessagePool[m]);
+
+            } else{
+                npcs[currChar].toRespondTo.Clear();
+            }
 
             currChar++;
         } else{ //your turn --> currChar == numChars
@@ -164,9 +446,45 @@ public class Main : Scene<TransitionData> {
     }
 
     public void ActionDoNothing(){
-        string message = "You did nothing";
+        string message = "Plot goes nowhere";
+        episodeNum++;
+        episodeNumText.text = ""+episodeNum;
         SendMessageToLog(message);
         currChar = 0;
+    }
+
+    public void ActionEveryoneDates(){
+        string message = "The audience is fed up with the filler episodes, so the characters confess their feelings for each other.";
+
+        episodeNum++;
+        episodeNumText.text = "" + episodeNum;
+        int counting = 0;
+        for (int i = 0; i < npcs.Count; ++i){
+            for (int r = 0; r < npcs[i].relationshipStates.Length;++r){
+                if (npcs[i].relationshipStates[r] != NPC.Relationship.Likes && counting == 0)
+                {
+                    counting++;
+                    message += " But they weren't all mutual. UNSATISFACTORY CONCLUSION. R to reboot series.";
+                }
+            }
+        }
+
+        if(!message.Contains("UNSATISFACTORY")){
+            message += " And they were all mutual. SATISFACTORY CONCLUSION. R to reboot series.";
+
+            episodeNum *= 10;
+            episodeNumText.text = "" + episodeNum;
+        }
+        nextButton.SetActive(false);
+        foreach (GameObject b in buttons)
+        {
+            b.SetActive(false);
+        }
+
+        gameEnd = true;
+
+        SendMessageToLog(message);
+        //currChar = 0;
     }
 
     public void SendMessageToLog(string msg){
